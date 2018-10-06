dd0e7c4c0baad915fa4dc027b50a00ab28303728
jefferson maquinana <jeffersonmaquinana@yahoo.com>
2018-10-06 17:04:52 +0800

    Testing branch

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Code name is " + coderName);
 		System.out.println("I am working in " + companyName);
+		System.out.print("Another for testing");
 
 	}
 
