eb70d000a0c26c1b6eb38f791b9410796115af29
jefferson maquinana <jeffersonmaquinana@yahoo.com>
2018-10-06 16:10:31 +0800

    Display coder Name.

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
+		String coderName = "Jefferson Maquinana";
+		
 		System.out.println("Hello World!");
+		System.out.println("Code name is " + coderName);
 
 	}
 
