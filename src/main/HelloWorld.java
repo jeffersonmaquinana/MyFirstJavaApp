9f643a2684ce9a5e1b1a66a6250e23c9e5a9601e
jefferson maquinana <jeffersonmaquinana@yahoo.com>
2018-10-06 17:01:22 +0800

    Adding helper

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -6,10 +6,12 @@ public class HelloWorld {
 		
 		String coderName = "Jefferson Maquinana";
 		String companyName = "Software Company";
+		Helper helper = new Helper();
 		
 		System.out.println("Hello World!");
 		System.out.println("Code name is " + coderName);
 		System.out.println("I am working in " + companyName);
+		System.out.println(helper.displayOtherInfo());
 
 	}
 
---
+++ src/main/Helper.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Helper {
+	
+	public String displayOtherInfo() {
+		return "This is practice only";
+	}
+}
