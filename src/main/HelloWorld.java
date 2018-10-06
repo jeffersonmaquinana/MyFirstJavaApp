33144d8b45d2187f87bb636d807b32f139168657
jefferson maquinana <jeffersonmaquinana@yahoo.com>
2018-10-06 16:17:49 +0800

    Display company name

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
+		String companyName = "Software Company";
+		
 		System.out.println("Hello World!");
+		System.out.println("I am working in " + companyName);
 
 	}
 
